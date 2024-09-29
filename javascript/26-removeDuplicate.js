d286d1cd65ede6828c271e89fc0a1a15c5f70611
nstlgy <yvsh7.btw@gmail.com>
2024-09-29 19:23:37 +0530

    solved

---
+++ javascript/26-removeDuplicate.js
@@ -0,0 +1,13 @@
+function removeDuplicates(nums) {
+  let index = 1;
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] !== nums[i + 1]) {
+      nums[index] = nums[i + 1];
+      index++;
+    }
+  }
+  console.log(nums);
+}
+
+const nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+removeDuplicates(nums);
