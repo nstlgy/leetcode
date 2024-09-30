2bb743dba2d8a6b054f86d9e167a355d360aba41
nstlgy <yvsh7.btw@gmail.com>
2024-09-30 22:42:31 +0530

    solved

---
+++ javascript/2723.js
@@ -0,0 +1,8 @@
+var addTwoPromises = async function (promise1, promise2) {
+  const res1 = await promise1;
+  const res2 = await promise2;
+  // console.log(res1, res2);
+  return res1 + res2;
+};
+
+addTwoPromises(Promise.resolve(2), Promise.resolve(2)).then(console.log); // 4
